c4a06cac2746e3dd445c4d3631930e3e6516608d
artem <artem@horizoninvestments.com>
2020-11-24 12:25:01 -0600

    Setup oryx for dual boot

--- oryxpro/configuration.nix
+++ oryxpro/configuration.nix
@@ -11,6 +11,10 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.hackeryarn = {
+    boot.loader.grub.enable = true;
+    boot.loader.grub.version = 2;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
     isNormalUser = true;
     home = "/home/hackeryarn";
     description = "Artem Chernyak";
