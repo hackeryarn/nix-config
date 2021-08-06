236f45dc8976595d193dc963b643b72c76bc5ba6
hackeryarn <artemchernyak@gmail.com>
2021-08-05 22:34:12 -0500

    Setup colors in tmux

--- home/.config/nixpkgs/programs/alacritty/default.nix
+++ home/.config/nixpkgs/programs/alacritty/default.nix
@@ -4,6 +4,7 @@
   programs.alacritty = {
     enable = true;
     settings = {
+      env = { TERM = "xterm-256color"; };
       selection.save_to_clipboard = true;
       shell.program = "${pkgs.fish}/bin/fish";
       colors = {
