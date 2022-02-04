9ffecf8f380947a42bc6bef9aa8abf53cf8a17e1
hackeryarn <artemchernyak@gmail.com>
2022-02-03 22:01:14 -0600

    Remove root gpg agent

--- system/shared.nix
+++ system/shared.nix
@@ -89,14 +89,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [ vim git wget stow ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-
   # Enable virtualization
   virtualisation = {
     libvirtd.enable = true;
